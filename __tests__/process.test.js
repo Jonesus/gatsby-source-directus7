f4a9acc691b3234d48d2eddd819419169ffa474f
jonesus <joonas.palosuo@gmail.com>
2019-06-04 20:51:36 +0300

    Add initial tests

--- __tests__/process.test.js
+++ __tests__/process.test.js
@@ -1,62 +1,5 @@
 import { prepareNodes, prepareFileNodes, mapRelations } from '../src/process';
 
-// Mock data for relation testing
-const entityData = {
-    movies: [
-        { id: 0, directusId: 0, name: 'Titanic', directors: 0 },
-        { id: 1, directusId: 1, name: 'Romeo & Juliet', directors: 1 },
-        { id: 2, directusId: 2, name: 'Bambi', directors: 1 },
-        { id: 4, directusId: 4, name: 'Wall-E', directors: 0 },
-    ],
-    directors: [
-        { id: 0, directusId: 0, name: 'John Smith' },
-        { id: 1, directusId: 1, name: 'Mary Sue' },
-    ],
-};
-
-const expectedData = {
-    movies: [
-        {
-            id: 0,
-            directusId: 0,
-            name: 'Titanic',
-            directors___NODE: 0,
-        },
-        {
-            id: 1,
-            directusId: 1,
-            name: 'Romeo & Juliet',
-            directors___NODE: 1,
-        },
-        {
-            id: 2,
-            directusId: 2,
-            name: 'Bambi',
-            directors___NODE: 1,
-        },
-        {
-            id: 4,
-            directusId: 4,
-            name: 'Wall-E',
-            directors___NODE: 0,
-        },
-    ],
-    directors: [
-        {
-            id: 0,
-            directusId: 0,
-            name: 'John Smith',
-            movies___NODE: [0, 4],
-        },
-        {
-            id: 1,
-            directusId: 1,
-            name: 'Mary Sue',
-            movies___NODE: [1, 2],
-        },
-    ],
-};
-
 describe('prepareNodes', () => {
     test('generates proper replacement IDs', () => {
         const data = {
@@ -79,12 +22,67 @@ describe('prepareFileNodes', () => {
 });
 
 describe('mapRelations', () => {
-    test('many-to-one works with proper relation', () => {
+    const entityData = {
+        movies: [
+            { id: 0, directusId: 0, name: 'Titanic', genres: 0 },
+            { id: 1, directusId: 1, name: 'Romeo & Juliet', genres: 1 },
+            { id: 2, directusId: 2, name: 'Bambi', genres: 1 },
+            { id: 4, directusId: 4, name: 'Wall-E', genres: 0 },
+        ],
+        genres: [
+            { id: 0, directusId: 0, name: 'Family' },
+            { id: 1, directusId: 1, name: 'Comedy' },
+        ],
+    };
+    const expected = {
+        movies: [
+            {
+                id: 0,
+                directusId: 0,
+                name: 'Titanic',
+                genres___NODE: 0,
+            },
+            {
+                id: 1,
+                directusId: 1,
+                name: 'Romeo & Juliet',
+                genres___NODE: 1,
+            },
+            {
+                id: 2,
+                directusId: 2,
+                name: 'Bambi',
+                genres___NODE: 1,
+            },
+            {
+                id: 4,
+                directusId: 4,
+                name: 'Wall-E',
+                genres___NODE: 0,
+            },
+        ],
+        genres: [
+            {
+                id: 0,
+                directusId: 0,
+                name: 'Family',
+                movies___NODE: [0, 4],
+            },
+            {
+                id: 1,
+                directusId: 1,
+                name: 'Comedy',
+                movies___NODE: [1, 2],
+            },
+        ],
+    };
+
+    test('handles many-to-one properly', () => {
         const relationData = [
             {
                 collection_many: 'movies',
-                field_many: 'directors',
-                collection_one: 'directors',
+                field_many: 'genres',
+                collection_one: 'genres',
                 field_one: null,
                 junction_field: null,
             },
@@ -95,53 +93,15 @@ describe('mapRelations', () => {
         const mappedEntities = mapRelations(entityData, relationData, []);
         console.log = originalLog;
 
-        expect(mappedEntities).toStrictEqual(expectedData);
+        expect(mappedEntities).toStrictEqual(expected);
     });
 
-    test('many-to-one attempts to fix broken "field_many"', () => {
+    test('handles one-to-many properly', () => {
         const relationData = [
             {
                 collection_many: 'movies',
-                field_many: null,
-                collection_one: 'directors',
-                field_one: null,
-                junction_field: null,
-            },
-        ];
-
-        const originalLog = console.log;
-        console.log = jest.fn();
-        const mappedEntities = mapRelations(entityData, relationData, []);
-        console.log = originalLog;
-
-        expect(mappedEntities).toStrictEqual(expectedData);
-    });
-
-    test('many-to-one fails gracefully with broken "collection_many"', () => {
-        const relationData = [
-            {
-                collection_many: null,
-                field_many: 'directors',
-                collection_one: 'directors',
-                field_one: null,
-                junction_field: null,
-            },
-        ];
-
-        const originalLog = console.log;
-        console.log = jest.fn();
-        const mappedEntities = mapRelations(entityData, relationData, []);
-        console.log = originalLog;
-
-        expect(mappedEntities).toStrictEqual(entityData);
-    });
-
-    test('one-to-many works with proper relation', () => {
-        const relationData = [
-            {
-                collection_many: 'movies',
-                field_many: 'directors',
-                collection_one: 'directors',
+                field_many: 'genres',
+                collection_one: 'genres',
                 field_one: 'movies',
                 junction_field: null,
             },
@@ -152,6 +112,6 @@ describe('mapRelations', () => {
         const mappedEntities = mapRelations(entityData, relationData, []);
         console.log = originalLog;
 
-        expect(mappedEntities).toStrictEqual(expectedData);
+        expect(mappedEntities).toStrictEqual(expected);
     });
 });
